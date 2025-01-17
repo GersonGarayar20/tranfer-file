3064256903b093cb13ead064b5555c5ee8e46a85
gerson <garayargerson20@gmail.com>
2025-01-17 16:33:24 -0500

    feat: new cambios

--- src/components/input-file.tsx
+++ src/components/input-file.tsx
@@ -2,6 +2,13 @@
 
 import { upload } from "@/lib/services/upload";
 import React, { useState } from "react";
+import {
+  CirclePlusIcon,
+  InfoIcon,
+  ArrowDownToLineIcon,
+  TrashIcon,
+  FileIcon,
+} from "lucide-react";
 
 export default function InputFile() {
   const [dragging, setDragging] = useState(false); // Estado para el estilo del área de arrastre
@@ -59,42 +66,73 @@ export default function InputFile() {
   };
 
   return (
-    <div className="relative w-full h-96 flex flex-col justify-center items-center p-4">
-      <div
-        onDragOver={handleDragOver}
-        onDragLeave={handleDragLeave}
-        onDrop={handleDrop}
-        className={`absolute inset-0 border-2 border-dashed border-gray-300 p-4 rounded-lg transition-all cursor-pointer hover:border-secondary ${
-          dragging ? "border-secondary" : ""
-        }`}
-        onClick={() => inputFileRef.current?.click()}
-      ></div>
+    <section className="p-6 w-full">
+      <h2 className="text-2xl font-semibold mb-6">Subir archivos</h2>
+      <div className="relative w-full h-96 flex flex-col justify-center items-center p-4">
+        <div
+          onDragOver={handleDragOver}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+          className={`absolute inset-0 border-2 border-dashed border-neutral-200 p-4 rounded-lg transition-all cursor-pointer hover:border-blue-200 ${
+            dragging ? "border-blue-200 bg-blue-200/10" : ""
+          }`}
+          onClick={() => inputFileRef.current?.click()}
+        ></div>
 
-      <p className="z-10 text-center">
-        {dragging
-          ? "Suelta los archivos aquí"
-          : "Arrastra archivos aquí o haz clic para seleccionar"}
-      </p>
+        <div className="flex flex-col justify-center items-center gap-3 text-neutral-500">
+          <CirclePlusIcon className="size-8" />
+          <p>Arrastre y suelte o haga clic para elegir archivos</p>
 
-      <input
-        type="file"
-        accept="image/*,.pdf,.doc,.docx,.txt"
-        multiple // Permitir múltiples archivos
-        onChange={handleInputChange}
-        className="hidden"
-        ref={inputFileRef}
-      />
-
-      {uploadedFiles.length > 0 && (
-        <div className="mt-4 text-gray-600">
-          <p>Archivos subidos:</p>
-          <ul>
-            {uploadedFiles.map((file, index) => (
-              <li key={index}>{file.name}</li>
-            ))}
-          </ul>
+          <div className="flex items-center gap-1">
+            <InfoIcon className="size-4" />
+            <p className="text-sm font-semibold">
+              Tamaño máximo de archivo: 10 MB
+            </p>
+          </div>
         </div>
-      )}
-    </div>
+
+        <input
+          type="file"
+          accept="image/*,.pdf,.doc,.docx,.txt"
+          multiple // Permitir múltiples archivos
+          onChange={handleInputChange}
+          className="hidden"
+          ref={inputFileRef}
+        />
+      </div>
+
+      <footer>
+        {uploadedFiles.length > 0 && (
+          <div className="flex flex-col gap-2 mt-6">
+            <ul>
+              {uploadedFiles.map((file, index) => (
+                <li
+                  key={index}
+                  className="flex items-center gap-3 border p-3 rounded-lg"
+                >
+                  <div className="p-2">
+                    <FileIcon className="size-6 text-orange-500" />
+                  </div>
+                  <div className="flex-1">
+                    <p>{file.name}</p>
+                    <span className="text-sm text-neutral-500">
+                      .{file.type.split("/")[1]} | {file.size} bytes
+                    </span>
+                  </div>
+                  <footer className="flex gap-2 items-center text-neutral-500">
+                    <button className="p-2 rounded-full border hover:text-blue-500">
+                      <ArrowDownToLineIcon className="size-4" />
+                    </button>
+                    <button className="p-2 rounded-full border hover:text-red-500">
+                      <TrashIcon className="size-4" />
+                    </button>
+                  </footer>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </footer>
+    </section>
   );
 }
